b00267a459090e224cee35221a6e36c9f753951f
ddong4 <ddong4@seas.upenn.edu>
2023-03-11 23:57:05 -0600

    js fundamentals pt1 data types

---
+++ 01-Fundamentals-Part-1/starter/assignments.js
@@ -0,0 +1,12 @@
+let country = 'USA';
+let continent = 'North America';
+let population = 2000;
+
+console.log(country, continent, population);
+
+// Lecture: Data types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country, typeof language);
---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,7 @@
+////////////////////////////////////
+// Linking a JavaScript File
+let js = 'amazing';
+if (js == 'amazing') alert('hi');
+console.log(40 + 8 + 23 - 10);
+
+console.log(typeof true);
